348d9d83da8a833957d54b34536301f2b65bcee9
Andrew <andrewxa9@yahoo.com>
2015-12-07 21:55:08 -0800

    added comments to tile system

--- Assets/Tile.cs
+++ Assets/Tile.cs
@@ -3,45 +3,67 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Tile : MonoBehaviour {
+
+	//neighboring tiles
 	public GameObject[] neighbors;
+	
+	//can tile be destroyed?
 	public bool destructible = true;
+	
 	[HideInInspector]
+	//minimal state machine value
 	public int fallState = 0;
 	
+	//this vector array is for translating 0 through 3 into vector directions
 	private Vector3[] directions = new Vector3[]{Vector3.forward,
 												 Vector3.left,
 												-Vector3.forward,
 												-Vector3.left};
 	
+	//visual properties for falling tiles
 	public Vector3 rotationSpeedBase;
 	public float rotationSpeedRandom = 5;
 	private Quaternion rotationSpeedFinal;
 	public float fallSpeed = 1.0f;
-	public float fallCoefficient = 1.0f;
+	public float fallCoefficient = 1.0f;//incorrect usage of coefficient but whatever
 	
+	//light source
 	public GameObject myLight;
+	
+	//cracks on tile
 	public GameObject alpha;
 	
+	//broken tile edges
 	public GameObject[] brokens;
+	
+	//audio source for breaking sound
 	public AudioSource myAudio;
+	//breaking sounds
 	public AudioClip[] breakSounds;
 	
 	void Wake() {
+	
+		//force debug drawing?
 		on = true;
 		
+		//creating random rotation values
 		rotationSpeedFinal.eulerAngles = rotationSpeedBase+new Vector3(Random.Range(-rotationSpeedRandom,rotationSpeedRandom),
 		                                                               Random.Range(-rotationSpeedRandom,rotationSpeedRandom),
 		                                                               Random.Range(-rotationSpeedRandom,rotationSpeedRandom));
-	
+		                                                               
+		//initialize neighbor array
 		neighbors = new GameObject[4];
 		
+		//find neighbors via raycast
 		RaycastHit hit;
-		int mask = 1 << 8;
+		int mask = 1 << 8; //only detect tiles
 		for(int i=0;i<directions.Length;i++){
 			if (Physics.Raycast(this.transform.position+(directions[i]*TileManager.gridSize)+Vector3.up,Vector3.down,out hit,5f,mask)){
+				//assign neighbor tile when hit
 				neighbors[i] = hit.collider.gameObject.transform.parent.gameObject;
 			}
 			else{
+				//no neighbor found
 				neighbors[i] = null;
 			}
 		}
@@ -50,12 +72,17 @@ public class Tile : MonoBehaviour {
 	}
 	
 	public void Update(){
+		
+		//0 = not falling, 1 = falling, 2 = fell
 		if(fallState == 1){
+		
+			//rotate and drop during fall
 			this.transform.position -= Vector3.up*fallSpeed*Time.deltaTime;
 			fallSpeed += fallCoefficient;
 			this.transform.rotation = this.transform.rotation*rotationSpeedFinal;
 			
 			if(this.transform.position.y < TileManager.lavaHeight){
+				//stop falling
 				fallState = 2;
 			}
 			
@@ -63,26 +90,36 @@ public class Tile : MonoBehaviour {
 		
 	}
 	
+	//for initiating falling, called from Beam.cs or TileManager.cs
 	public void breakTile(){
+		//check if destructible
 		if(destructible){
+			//change state, activate light, play sound
 			fallState = 1;
 			myLight.active = true;
 			myLight.transform.parent = null;
 			myAudio.clip = breakSounds[(int)Mathf.Floor(Random.value*(breakSounds.Length))];
 			myAudio.Play();
 		}
+		//send lighting check to neighbors
 		for(int i=0;i<4;i++){
 			if(neighbors[i] != null){
 				neighbors[i].SendMessage("LightingCheck");
 			}
 		}
+		//check own lighting (y'know, just in case)
 		this.gameObject.SendMessage("LightingCheck");
 		
 	}
+	
+	//for checking if tile is broken and has no neighbors to turn off light
 	public void LightingCheck(){
+	
+		//variable to see if there are neighbors left
 		int keep = 0;
 		for(int i=0;i<4;i++){
 			if(neighbors[i] != null){
+				//iterate thru neighbors and check their fall states
 				if(neighbors[i].GetComponent<Tile>().fallState == 0){
 					keep += 1;
 				}
@@ -94,16 +131,19 @@ public class Tile : MonoBehaviour {
 				}
 			}
 		}
+		//if no neighbors, slowly and sadly disable light (see LightToggle.cs)
 		if(keep == 0){
 			myLight.SendMessage("FadeOut",SendMessageOptions.DontRequireReceiver);
 			//Debug.Log("sent");
 		}
 	}
 	
+	//toggle for debug gizmos
 	private bool on = false;
 	
 	void OnDrawGizmos() {
 		
+		//draw connections to neighbor tiles
 		Gizmos.color = Color.red;
 		if (on){
 			for(int i=0;i<4;i++){
@@ -114,6 +154,7 @@ public class Tile : MonoBehaviour {
 		}
 	}
 	
+	//Helper class to convert from int direction to quaternion
 	Quaternion DirToQuat(int dir){
 		float deg = 90;
 		for(int i=0;i<dir;i++){
--- Assets/TileManager.cs
+++ Assets/TileManager.cs
@@ -2,42 +2,64 @@
 using System.Collections;
 using System.Collections.Generic;
 
+//this is where the heatmap data is stored and calculated
 public class TileManager: MonoBehaviour {
+
+	//container for all tile heat data
 	public List<HeatMapEntity> tiles;
+	
+	//size of tiles
 	public static float gridSize = 2.0f;
+	
+	//elevation for tiles to fall
 	public static float lavaHeight = -10.0f;
+	//base strength of tile heat
 	public float heatStrength = 0.01f;
+	//distance from tile to player to activate heat
 	public float heatDistance = 5f;
+	//curve to adjust strength of heat from player over distance
 	public AnimationCurve heatFalloff;
+	//heat limit, tiles break when exceeding this
 	public float heatLimit = 5f;
+	//player object
 	private GameObject player;
 	
 	public List<Material> cracks;
 	
 	void Awake () {
+		//find player
 		player = GameObject.FindGameObjectWithTag("Player");
-		tiles = new List<HeatMapEntity>();
+		
 	}
 	
 	void FixedUpdate(){
+		//if there are still tiles left
 		if(tiles.Count > 0){
-			//Debug.Log("greater than 0");
+		
+			//store any tile indicies that break
 			List<HeatMapEntity> deadTiles = new List<HeatMapEntity>();
+			
+			//iterate through enabled tile list
 			for(int i = 0;i<tiles.Count;i++){
+			
+				//get player distance to tile
 				float playerDist = (tiles[i].tile.transform.position-player.transform.position).magnitude;
 				if((playerDist < heatDistance)&&(tiles[i].properties.destructible)){
+					//apply heat equasion to tile
 					tiles[i].heat += heatStrength*heatFalloff.Evaluate(playerDist/heatDistance);
-					
+					//crack tiles according to heat
 					tiles[i].properties.alpha.GetComponent<Renderer>().material = cracks[Mathf.Clamp((int)Mathf.Ceil((tiles[i].heat/heatLimit)*(cracks.Count-1)),0,cracks.Count-1)];
 					
 				}
 				if(tiles[i].heat >= heatLimit){
+					//break tiles that exceeded heat limit, initialize their fall logic
 					tiles[i].tile.gameObject.SendMessage("breakTile");
 					deadTiles.Add(tiles[i]);
 					player.transform.position += Vector3.up*Random.Range(0.1f,0.2f)*4;
 					//Debug.Log("Kill");
 				}
 			}
+			//remove all broken tiles from list so we don't waste time applying heat
 			foreach(HeatMapEntity i in deadTiles){
 				tiles.Remove(i);
 			}
@@ -54,17 +76,21 @@ public class TileManager: MonoBehaviour {
 		}
 	}
 	
-	
+	//populate tile list, called from Generate.cs
 	void FindTiles(){
 		Debug.Log("Finding Tiles");
+		//initialize tile list
 		tiles = new List<HeatMapEntity> ();
+		//make temp list for found tiles
 		GameObject[] aTiles = GameObject.FindGameObjectsWithTag ("Tile");
+		//make noew hearmap entity for every tiles
 		foreach(GameObject i in aTiles){
 			tiles.Add(new HeatMapEntity(i,0,i.GetComponent<Tile>()));
 			i.SendMessage("Wake");
 			//Debug.Log("added");
 		}
 		
+		//lock tiles near origin where player spawns
 		foreach(HeatMapEntity i in tiles){
 			if(i.tile.gameObject.transform.position.magnitude < heatDistance){
 				i.tile.GetComponent<Tile>().destructible = false;
@@ -75,6 +101,7 @@ public class TileManager: MonoBehaviour {
 	
 }
 
+//small helper class for storing tile and tile's heat data
 [System.Serializable]
 public class HeatMapEntity{
 	public GameObject tile;
